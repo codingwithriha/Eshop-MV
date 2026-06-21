b2c3dfb2e74e51b870d46de9659200f0e793574c
Codingwithriha <rihashehzadi2003@gmail.com>
2026-06-22 01:13:02 +0500

    Refactor CORS middleware configuration and clean up app.js

--- backend/app.js
+++ backend/app.js
@@ -5,24 +5,21 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-app.use(cors());
-
-
-
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use("/", express.static("uploads"));
-app.use("/test", (req, res) => {
-  res.send("Hello world!");
-});
-
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-
 
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
-    path: "config/.env",
+    path: "backend/config/.env",
   });
 }
 
@@ -34,4 +31,4 @@ app.use("/api/v2/user", user);
 // it's for ErrorHandling
 app.use(ErrorHandler);
 
-module.exports=app;
+module.exports = app;
